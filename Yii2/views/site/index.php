85cda9efcf62043d06f26b41fb00c6f5a52913f6
Наталья <@DESKTOP-QA65QKN.Dlink>
2020-04-14 09:53:58 +0600

    непонятки с css файлом

--- Yii2/views/site/index.php
+++ Yii2/views/site/index.php
@@ -41,11 +41,11 @@ $this->title = SITE_TITLE;
             <li data-target="#mainCarousel" data-slide-to="0" class="active"></li>
             <li data-target="#mainCarousel" data-slide-to="1"></li>
             <li data-target="#mainCarousel" data-slide-to="2"></li>
-            <li data-target="#mainCarousel" data-slide-to="2"></li>
-            <li data-target="#mainCarousel" data-slide-to="2"></li>
-            <li data-target="#mainCarousel" data-slide-to="2"></li>
-            <li data-target="#mainCarousel" data-slide-to="2"></li>
-            <li data-target="#mainCarousel" data-slide-to="2"></li>
+            <li data-target="#mainCarousel" data-slide-to="3"></li>
+            <li data-target="#mainCarousel" data-slide-to="4"></li>
+            <li data-target="#mainCarousel" data-slide-to="5"></li>
+            <li data-target="#mainCarousel" data-slide-to="6"></li>
+            <li data-target="#mainCarousel" data-slide-to="7"></li>
         </ol>
 
 
